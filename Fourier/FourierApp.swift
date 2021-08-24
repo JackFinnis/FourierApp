d335b6ff1df84efaf5971febc3afb6b9acc7bd6a
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-24 11:41:54 +0100

    Initial Commit

---
+++ Fourier/FourierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourierApp.swift
+//  Fourier
+//
+//  Created by Jack Finnis on 24/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FourierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
